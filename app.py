bf3055647ebfc3b91cc550b8460a629c7515e2c8
Yiyang~ <yge@feitian.com>
2024-11-27 15:47:11 -0500

    book dir

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from llama_index.llms.groq import Groq
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from dotenv import load_dotenv
 import os
+from llama_index.core.agent import ReActAgent
+
 
 groq_api = str(os.environ.get("GROQ_API_KEY"))
 
@@ -16,3 +18,5 @@ embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 Settings.llm = llm
 Settings.embed_model = embed_model
 Settings.chunk_size = 512
+
+textbook_1 = SimpleDirectoryReader(input_files=["books/theory_never_die.pdf"]).load_data()
