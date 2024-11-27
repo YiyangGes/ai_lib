1f49dbd2bc7a99ad848747f4d404646213449598
Yiyang~ <yge@feitian.com>
2024-11-27 15:36:31 -0500

    set api

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ from llama_index.core import (
     VectorStoreIndex,
     SimpleDirectoryReader
 )
+from llama_index.llms.groq import Groq
+from dotenv import load_dotenv
+import os
+
+groq_api = str(os.environ.get("GROQ_API_KEY"))
+
+llm = Groq(model="llama3-groq-70b-8192-tool-use-preview", api_key=groq_api)
